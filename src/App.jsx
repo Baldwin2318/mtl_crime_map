c42f93e5b802980329d525a4f14dd3573c4cddff
baldwin <baldwinmalabanan.bm@gmail.com>
2025-12-06 11:45:12 -0500

    App.jsx: Change layer line into dash ones

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const URL_PDQ_1 = 'https://donnees.montreal.ca/dataset/186892b8-bba5-426c-aa7e-9
 const URL_PDQ_2 = '/limitespdq_wgs84.geojson'
 const URL_PDQ_NAME = 'https://montreal-prod.storage.googleapis.com/resources/c9d0b8d6-c7a6-4766-a5cc-98e8b1392bbc/pdq.geojson?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=test-datapusher-delete%40amplus-data.iam.gserviceaccount.com%2F20251206%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20251206T154115Z&X-Goog-Expires=604800&X-Goog-SignedHeaders=host&x-goog-signature=32295069d98f111f332922f27e717291a89f2c1f1ba0686d79a278128d2e63c78a85db266135d75ec2248840bc187c3d8bd5b86cffc3dadb3b258fbabb8a65cbfd85d73eab454627b9b7006c441a3ba929083d99287e3756dc335d287d24c9946b5838cfc6bd492520ceca3a6652ff58475f8419d3bd9c59ea7124e99ee520408d0abec98ee343f6dc6aad3e28f1969ec29769826de5a070a4c30b0f794c12189b3659dc2d82939813401d4b2927a93e5ce285aa2f5d7d684747b856b2f4165a74da05c9cc79eee41edae077e0168a7d1f901a7e212dcd02001440e8e1116a78925daa1455b4d280d0721e9b9b5033bbaf81c2d3b88e288e9014ee59630dbf10'
 
-const PDQ_BASE_STYLE = { weight: 1, color: '#333', fillOpacity: 0, }
+const PDQ_BASE_STYLE = { weight: 3, color: '#333333ff', dashArray: '4 4', fillOpacity: 0, };
 const PDQ_HOVER_STYLE = {  weight: 3, color: '#2563eb', fillOpacity: 0.8, }
 
 export default function App() {
